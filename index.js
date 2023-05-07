29e2e5524297f53591a7ea1f907741ea42ff58a6
Adam Shalala <adamshalala@192-168-1-119.tpgi.com.au>
2023-05-07 11:09:54 +1000

    created schema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 // import and require inquirer
-const inquirer = require('inquirer';)
+const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
 
