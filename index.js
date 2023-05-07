ffaa0fa3390d312c8e074b19f157359cd411ccec
Adam Shalala <adamshalala@192-168-1-119.tpgi.com.au>
2023-05-07 10:47:54 +1000

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// import and require fs
+const fs = require('fs');
+
+// import and require inquirer
+const inquirer = require('inquirer';)
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
