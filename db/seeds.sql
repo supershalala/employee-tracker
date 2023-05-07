8ccc696bb5e1fefecbdfa17ebc0490b89f6e5c26
Adam Shalala <adamshalala@192-168-1-119.tpgi.com.au>
2023-05-07 15:16:44 +1000

    view departmetns func

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"),
+       (2, "marketing"),
+       (3, "Finance")
--- index.js
+++ index.js
@@ -19,9 +19,31 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
   );
 
+  const functions = {
+    'View all departments': viewDepartments,
+    // 'View all roles': viewRoles,
+    // 'View all employees': viewEmployees,
+    // 'Add a department': addDepartment,
+    // 'Add a role': addRole,
+    // 'Add an employee': addEmployee,
+    // 'Update an employee': updateEmployee
+  };
+  
   inquirer.prompt({
     type: 'list',
-    name: 'options',
-    message: 'Weclome to the Employee DB, please select from the below to begin!',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee']
-  })
+    name: 'option',
+    message: 'Welcom to the Employee DB, please select an option:',
+    choices: Object.keys(functions)
+  }).then((answer) => {
+    const func = functions[answer.option];
+    func(db);
+  });
+  
+  function viewDepartments(connection) {
+    connection.query('SELECT * FROM department', (err, results) => {
+      if (err) throw err;
+      console.table(results, ['id', 'name']);
+      connection.end();
+    });
+  }
+  
