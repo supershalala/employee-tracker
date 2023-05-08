018b7d786eb245a06e9b5c19b07cd8cd4848fc68
Adam Shalala <adamshalala@192-168-1-119.tpgi.com.au>
2023-05-09 08:50:39 +1000

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,15 @@
-INSERT INTO department (id, name)
-VALUES (1, "Sales"),
-       (2, "marketing"),
-       (3, "Finance")
+INSERT INTO department (name)
+VALUES ( "Sales"),
+       ( "marketing"),
+       ( "Finance");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager","100000",2),
+        ("Coordinator", "80000", 1),
+        ("Junior", "60000", 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES("Adam", "Shalala",1, NULL),
+        ("Jane", "Doe", 2, 1),
+        ("Nicholas", "Davidson", 3, 2);
